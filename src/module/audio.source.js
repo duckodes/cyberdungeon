94d8e5f6acd249d695808a7932f4dcdeae96fb51
duckode <73285767+duckodes@users.noreply.github.com>
2026-09-16 14:43:50 +0800

    Update audio.source.js

--- src/module/audio.source.js
+++ src/module/audio.source.js
@@ -24,18 +24,18 @@ const audioSource = (() => {
     */
     function BGM() {
         audioUtils.loadBackgroundMusic([
-            '../src/audio/bgm/audio1.mp3',
-            '../src/audio/bgm/audio2.mp3'
+            './src/audio/bgm/audio1.mp3',
+            './src/audio/bgm/audio2.mp3'
         ]);
     }
     /**
     * Load sound effects source
     */
     function FX() {
-        audioUtils.loadSoundEffect('click1', '../src/audio/click/click1.mp3');
-        audioUtils.loadSoundEffect('click2', '../src/audio/click/click2.mp3');
-        audioUtils.loadSoundEffect('click3', '../src/audio/click/click3.mp3');
-        audioUtils.loadSoundEffect('click4', '../src/audio/click/click4.mp3');
+        audioUtils.loadSoundEffect('click1', './src/audio/click/click1.mp3');
+        audioUtils.loadSoundEffect('click2', './src/audio/click/click2.mp3');
+        audioUtils.loadSoundEffect('click3', './src/audio/click/click3.mp3');
+        audioUtils.loadSoundEffect('click4', './src/audio/click/click4.mp3');
     }
     function playSoundEffect(name) {
         audioUtils.playSoundEffect(name);
