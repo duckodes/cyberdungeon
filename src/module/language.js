5515bdd6497fa13d4d65a7916f4faceaa31337ea
duckode <73285767+duckodes@users.noreply.github.com>
2026-09-16 14:40:07 +0800

    fix language.js url

--- src/module/language.js
+++ src/module/language.js
@@ -4,12 +4,12 @@ const language = (() => {
     let data = null;
 
     async function set(lang) {
-        data = await fetcher.load(`../src/lan/${lang}.json`).catch(() => fetcher.load(`../src/lan/en-ZA.json`));
+        data = await fetcher.load(`../src/lan/${lang}.json`).catch(() => fetcher.load(`./src/lan/en-ZA.json`));
         return data;
     }
 
     async function cache(lang) {
-        data ||= await fetcher.load(`../src/lan/${lang}.json`).catch(() => fetcher.load(`../src/lan/en-ZA.json`));
+        data ||= await fetcher.load(`../src/lan/${lang}.json`).catch(() => fetcher.load(`./src/lan/en-ZA.json`));
         return data;
     }
 
